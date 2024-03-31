267601fc953c6b48e5bd22156b2461c72db4669f
jihyeonjeong11 <jihyeonjeong1117@gmail.com>
2024-03-31 22:45:20 +0900

    fix setting

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,16 +8,16 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://jihyeonjeong11.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/mydoc/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "jihyeonjeong11", // Usually your GitHub org/user name.
   projectName: "mydoc", // Usually your repo name.
-  deploymentBranch: "main",
+  deploymentBranch: "gh-page",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
